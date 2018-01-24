9c6ba253c544ef3f95dcab30fb7492be0c167eb3
Developer <mr.campbell.hawkes@gmail.com>
2018-01-24 17:48:38 +1300

    Initial slider concept

--- src/client/app/app.jsx
+++ src/client/app/app.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter} from 'react-router-dom';
 
 import site from './siteScaffold.js';
 
@@ -13,45 +13,106 @@ import ArtContainer from './components/art/index.jsx';
 import Gallery from './components/gallery/index.jsx';
 import About from './components/about/index.jsx';
 import Contact from './components/contact/index.jsx';
+import Styles from './app.css';
 import './css/normalize.css';
 import './css/base.css';
+import Routes from './routes.js';
 
 
 
-
-export default class App extends Component {
+class App extends Component {
    constructor(props){
       super(props)
+      this.state = {
+         ...props,
+         width: '100%',
+         routes: Routes,
+         sliderOffset: 0,
+         sliderWidth: Routes.length * 100 + "%",
+         route: props.history.location.pathname
+      };
+      this.updateDimensions = this.updateDimensions.bind(this);
+      this.updateDimensions();
+      this.changeRoute = this.changeRoute.bind(this);
+      this.initialSlider = this.initialSlider.bind(this);
+   }
+   changeRoute(path = this.state.route){
+      path = path.split('/')[0] 
+      console.log("ChangeRoute:", path)
+      for (var z=0; z < Routes.length; z++) {
+         if (Routes[z][1] == path) {
+            this.initialSlider(z)
+         }
+      }
+   }
+   updateDimensions() {
+
+            var w = window, 
+                d = document, 
+                documentElement = d.documentElement,
+               body = d.getElementsByTagName('body')[0],
+               width = w.innerWidth || documentElement.clientWidth || body.clientWidth,
+               height = w.innerHeight|| documentElement.clientHeight|| body.clientHeight,
+               sliderWidth = this.state.routes.length * width;
+            console.log("NewSize:", width, height)
+            this.setState({width: width, height: height, sliderWidth: sliderWidth
+            });
+      
+   }         
+   componentDidMount() {
+       window.addEventListener("resize", this.updateDimensions)
+      this.props.history.listen( location =>  {
+         this.changeRoute(location.pathname) 
+      });
+      this.updateDimensions();
+      
+      this.initialSlider();
+      this.changeRoute();
+   }
+   componentWillUnmount() {           
+      window.removeEventListener("resize", this.updateDimensions);
+   }
+   initialSlider(offset = 0){
+      console.log("INITIALSLIDERFUNC: Offset", offset, this.state.routes.length);
+      let sliderOffset = offset * this.state.width;
+      let sliderWidth = this.state.routes.length * this.state.width;
+      console.log("sliderOffset", sliderOffset, "sliderWidth", sliderWidth, "Width", this.state.width, "RoutesLength", this.state.routes.length)
+      this.setState({
+         sliderWidth: sliderWidth,
+         sliderOffset: sliderOffset
+      })
    }
    componentWillReceiveProps(nextProps){
-      if (this.props !== nextProps){
-         nextProps.load()
+      if (this.props.history !== nextProps.history){
+         this.changeRoute() 
       }
    }
    render () {
+      let sliderStyles = {
+         width: this.state.sliderWidth,
+         left:  this.state.sliderOffset
+      }
+      console.log(sliderStyles, this.state.width)
       return (
             <Layout>  
                <div style={{zIndex: 2}}>
                   <Header />
-                  <Content>
-                     <Switch>
-                        <Route exact path={"/"}>
-                           <Home>
-                              <Opener opener={site.home.opener} />
-                              <Gallery items={site.home.gallery} />
-                           </Home>
-                        </Route>
-                        <Route path="/about">
-                           <About content={site.about}/>
-                        </Route>
-                        <Route path="/contact">
-                           <Contact content={site.contact} /> 
-                        </Route>
-                     </Switch>
-                  </Content>
+                     <Content>
+                        <div className={Styles.sliderContainer}>
+                           <div className={Styles.sliderContent} style={sliderStyles}>
+                              <Home>
+                                 <Opener opener={site.home.opener} />
+                                 <Gallery items={site.home.gallery} />
+                              </Home>
+                              <About content={site.about}/>
+                              `<Contact content={site.contact} /> 
+                           </div>
+                        </div>
+                     </Content>
                   <Footer owner={site.owner} />
                </div>
             </Layout>
       )
    }
-}
+};
+export default withRouter(App);
--- src/client/app/routes.js
+++ src/client/app/routes.js
@@ -1,16 +1,6 @@
 const Routes = [
    ["HOME", "home"],
    ["ABOUT", "about"],
-   ["VISUAL ART", "art",
-      [
-         ["all personal work", "all"],
-         ["painting", "painting"],
-         ["sculpture", "sculpture"],
-         ["other creations", "other"],
-         ["commission", "commission"]
-      ]
-   ],
-   ["WORDS", "words"],
    ["CONTACT", "contact"]
 ]
 export default Routes;
